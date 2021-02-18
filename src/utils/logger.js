3e3bc761342f325b0e41723381826c648f710b58
Andrea Tombolato <andreacw96@gmail.com>
2021-02-18 10:44:48 +0100

    New logger transporter

---
+++ src/utils/logger.js
@@ -0,0 +1,53 @@
+const { createLogger, format, transports } = require('winston');
+
+const { timestamp } = format;
+
+require('winston-daily-rotate-file');
+
+const enumerateErrorFormat = format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+// eslint-disable-next-line no-shadow
+const logFormat = format.printf(({ timestamp, level, message, ...metadata }) => {
+  let msg = `${timestamp} [${level}]: ${message} `;
+  if (metadata && Object.keys(metadata).length) {
+    msg += JSON.stringify(metadata);
+  }
+  return msg;
+});
+
+const fileWriterTransport = new transports.DailyRotateFile({
+  filename: 'logs/element-api-%DATE%.log',
+  datePattern: 'YYYYMMDD',
+  zippedArchive: true,
+  maxSize: 5242880,
+  maxFiles: 10,
+  level: 'info',
+  json: true,
+  format: format.combine(enumerateErrorFormat(), format.uncolorize(), timestamp(), format.splat(), logFormat),
+});
+
+const logger = createLogger({
+  level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+  format: format.combine(enumerateErrorFormat(), format.colorize(), timestamp(), format.splat(), logFormat),
+  transports: [
+    new transports.Console({
+      level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+    }),
+    fileWriterTransport,
+  ],
+  exitOnError: false,
+});
+
+fileWriterTransport.on('rotate', function (oldFilename, newFilename) {
+  logger.log('info', 'Log file rotation', {
+    oldFilename,
+    newFilename,
+  });
+});
+
+module.exports = logger;
